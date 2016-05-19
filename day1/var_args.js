e6186d4deff22c9b4e6340ccd437465093f94ec9
joshpaulchan <joshpaulchan@gmail.com>
2016-05-19 10:32:59 -0400

    Remove implementation notes

--- day1/var_args.js
+++ day1/var_args.js
@@ -12,7 +12,6 @@ window.varArgs = {};
 // Hint: see <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments> for details about the `arguments` data structure
 varArgs.numArgs = function() {
   // TODO: YOUR CODE HERE
-  return arguments.length;
 };
 
 
@@ -24,21 +23,6 @@ varArgs.numArgs = function() {
 //
 varArgs.sum = function() {
   // TODO: YOUR CODE HERE
-  if (arguments.length == 0) {
-    return 0;
-  }
-  
-  // 1. iterative
-  var sum = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
-  }
-  return sum;
-  
-  // 2. functional-ish
-  // return Array.prototype.slice.call(arguments).reduce(function(a, b) {
-  //   return a + b;
-  // }, 0);
 };
 
 // Exercise 2. varArgs.product(args...)
@@ -49,21 +33,6 @@ varArgs.sum = function() {
 //
 varArgs.product = function() {
   // TODO: YOUR CODE HERE
-  if (arguments.length == 0) {
-    return 0;
-  }
-  
-  // 1. iterative
-  var prod = 1;
-  for (var i = 0; i < arguments.length; i++) {
-    prod *= arguments[i];
-  }
-  return prod;
-  
-  // 2. functional-ish
-  // return Array.prototype.slice.call(arguments).reduce(function(a, b) {
-  //   return a * b;
-  // }, 1);
 };
 
 // Exercise 3. varArgs.joinWith(args...)
@@ -73,25 +42,4 @@ varArgs.product = function() {
 //
 varArgs.joinWith = function() {
   // TODO: YOUR CODE HERE
-  if (arguments.length == 0) {
-    return '';
-  }
-  
-  // 1. iterative
-  var delimiter = arguments[0];
-  var word = "";
-  for (var i = 1; i < arguments.length; i++) {
-    word += arguments[i];
-    if (i != arguments.length - 1) {
-      word += delimiter;
-    }
-  }
-  return word;
-  
-  // 3. functional-ish
-  var args = Array.prototype.slice.call(arguments);
-  var delim = args[0];
-  return args.reduce(function(a, b) {
-    return a + delim + b;
-  }, 1);
 };

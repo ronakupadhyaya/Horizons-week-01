cf1469732c31d8307ed2386b6644d4f24c88bb7d
joshpaulchan <joshpaulchan@gmail.com>
2016-05-24 20:37:36 -0400

    Fix issues on PR

--- day2/dates.js
+++ day2/dates.js
@@ -13,56 +13,58 @@ dates.createDate = function(dateStr) {
   // YOUR CODE HERE
 };
 
-// Exercise 2. dates.getUTCString(d<Date>)
+// Exercise 2. dates.getUTCString(dateObj<Date>)
 // Write a function that takes a Date Object and returns the UTC time string.
 // ex. dates.getUTCString('May 17, 2016 9:00:00') -> Date(2016, 4, 17, 9, 0, 0, 0).toUTCString()
 // ex. dates.getUTCString('2015-03-25') -> Date('2015-03-25').toUTCString()
 // ex. dates.getUTCString('2015-03-25T12:00:00') -> Date('2015-03-25T12:00:00').toUTCString()
 //
+// note. UTC is kiiiinda like GMT - it's just another format of time
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toUTCString
 // hint. see http://www.w3schools.com/js/js_dates.asp
-dates.getUTCString = function(d) {
+dates.getUTCString = function(dateObj) {
   // YOUR CODE HERE
 };
 
-// Exercise 3.A dates.isSameDayOfWeek(d<Date>, c<Date>)
+// Exercise 3.A dates.isSameDayOfWeek(dateObj<Date>, otherDateObj<Date>)
 // Write a function that takes two Date Objects as arguments and returns true if both dates occur on the same day of the week (both on Tuesdays, etc.), false otherwise
 // ex. dates.isSameDayOfWeek(new Date('2015-03-25'), new Date('2015-03-25')) -> true
 // ex. dates.isSameDayOfWeek(new Date('2015-03-25'), new Date('2015-10-25')) -> true
 // ex. dates.isSameDayOfWeek(new Date('2015-03-25'), new Date('2015-04-25')) -> false
 //
-// hint. see http://www.w3schools.com/js/js_dates.asp
-dates.isSameDayOfWeek = function(d, c) {
+// hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
+dates.isSameDayOfWeek = function(dateObj, otherDateObj) {
   // YOUR CODE HERE
+  return dateObj.getDay() === otherDateObj.getDay();
 };
 
-// Exercise 3.B dates.isSameTimeOfDay(d<Date>, c<Date>)
+// Exercise 3.B dates.isSameTimeOfDay(dateObj<Date>, otherDateObj<Date>)
 // Write a function that takes two Date Objects as arguments and returns true if both dates occur on the same time of day (both at 3:03 AM, etc.), false otherwise
-// ex. dates.isSameDayOfWeek(new Date('2015-03-25 02:00:00'), new Date('2015-03-25 02:00:00')) -> true
-// ex. dates.isSameDayOfWeek(new Date('2015-03-25'), new Date('2015-10-25')) -> true
-// ex. dates.isSameDayOfWeek(new Date('2015-03-25'), new Date('2015-04-25')) -> false
+// ex. dates.isSameTimeOfDay(new Date('2015-03-25 02:00:00'), new Date('2015-03-25 02:00:00')) -> true
+// ex. dates.isSameTimeOfDay(new Date('2015-03-25'), new Date('2015-10-25')) -> true
+// ex. dates.isSameTimeOfDay(new Date('2015-03-25'), new Date('2015-04-25')) -> false
 // 
-// hint. see http://www.w3schools.com/js/js_dates.asp
-dates.isSameTimeOfDay = function(d, c) {
+// hint. don't worry about milliseconds!
+// hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getHours
+dates.isSameTimeOfDay = function(dateObj, otherDateObj) {
   // YOUR CODE HERE
 };
 
-// Exercise 3.C dates.isTheFuture(d<Date>)
+// Exercise 3.C dates.isTheFuture(dateObj<Date>)
 // Write a function that takes a Date object as an argument and returns true if it specifies a date in the future or false if it is a time that has already passed
 // 
-// hint. see Date.now() and parseInt
-// hint. see http://www.w3schools.com/js/js_dates.asp
-dates.isTheFuture = function(d) {
+// hint. how do you check if something is 'bigger than' something else?
+dates.isTheFuture = function(dateObj) {
   // YOUR CODE HERE
 };
 
-// Exercise 4. dates.incrementDay(d<Date>)
+// Exercise 4. dates.incrementDay(dateObj<Date>)
 // Write a function that takes a Date Object and treturns the UTC time string.
 // ex. dates.incrementDay() -> false
 // 
 // hint. don't worry about overflow!
 // hint. see 'getDate' and 'setDate'
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setDate
-dates.incrementDay = function(d) {
+dates.incrementDay = function(dateObj) {
   // YOUR CODE HERE
 };

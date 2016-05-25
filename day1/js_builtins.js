594301e0240441cffbec178bb4f6cceb540ae0df
Ethan Lee <ethanlee@ethansites.com>
2016-05-25 05:39:23 -0700

    Updated exercises per comments

--- day1/js_builtins.js
+++ day1/js_builtins.js
@@ -33,6 +33,10 @@ builtins.trim = function(string) {
 
 // ex. builtins.search('Horizons', 'o') -> true
 // ex. builtins.search('Horizons', 'oz') -> false
+// ex. builtins.search('rizo', 'Horizons') -> false
+// ex. builtins.search('', 'Horizons') -> false
+// ex. builtins.search('Horizons', '') -> true
+// ex. builtins.search('Horizons', 'h') -> false
 
 builtins.search = function(sourceString, searchString) {
   // YOUR CODE HERE
@@ -48,6 +52,11 @@ builtins.search = function(sourceString, searchString) {
 
 // ex. builtins.parseQuantity('1 tool') -> 1
 // ex. builtins.parseQuantity('8 buckets') -> 8
+// ex. builtins.parseQuantity('0 computers') -> 0
+
+// Hint: Use split() to separate each part of the input string by spaces
+// (or any other separator). See:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
 builtins.parseQuantity = function(string) {
   // YOUR CODE HERE
@@ -62,6 +71,8 @@ builtins.parseQuantity = function(string) {
 
 // ex. builtins.reverse([1, 2, 3]) -> [3, 2, 1]
 // ex. builtins.reverse(['dogs', 'cats', 'moose']) -> ['moose', 'cats', 'dogs']
+// ex. builtins.reverse([]) -> []
+// ex. builtins.reverse([123]) -> [123]
 
 builtins.reverse = function(array) {
   // YOUR CODE HERE
@@ -77,6 +88,9 @@ builtins.reverse = function(array) {
 // ex. builtins.isEqual([1, 2, 3], [1, 2, 3]) -> true
 // ex. builtins.isEqual(['1', '2', '3'], [1, 2, 3]) -> false
 // ex. builtins.isEqual([3, 2, 1], [1, 2, 3]) -> false
+// ex. builtins.isEqual([], [1, 2, 3]) -> false
+// ex. builtins.isEqual([1, 2, 3], []) -> false
+// ex. builtins.isEqual([], []) -> true
 
 builtins.isEqual = function(a, b) {
   // YOUR CODE HERE
@@ -87,8 +101,8 @@ builtins.isEqual = function(a, b) {
 // Exercise 6. Checking if an array is a palindrome (forward order is the same
 // as reversed order).
 
-// Write a function that takes an array a and checks if the original order of
-// its contents as it is reversed.
+// Write a function that takes an array a and checks if the order of its contents
+// in reverse is identical to the original order of the contents.
 
 // ex. builtins.isPalindrome([1, 2, 3, 2, 1]) -> true
 // ex. builtins.isPalindrome([1, 2, 3, 4, 5]) -> false
@@ -101,15 +115,38 @@ builtins.isPalindrome = function(a) {
 
 // ----------------------------------------------------------------------------
 
-// Exercise 7. Sorting a 2D array based on the length of its subarrays.
+// Exercise 7. Sorting an array of numbers in ascending order.
+
+// Write a function that takes an array of numbers and returns the
+// array sorted by ascending numerical value.
+
+// ex. builtins.sortByValue([10, 1, 5, 4]) -> [1, 4, 5, 10]
+// ex. builtins.sortByValue([1, 2, 3]) -> [1, 2, 3]
+// ex. builtins.sortByValue([0, -6, -6]) -> [-6, -6, 0]
+
+// Hint: Use the built-in Array sort() function with a compare function
+// to sort by numerical value instead of by Unicode point value (the default
+// behavior). See: 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+builtins.sortByValue = function(array) {
+  // YOUR CODE HERE
+};
+
+// ----------------------------------------------------------------------------
+
+// Exercise 8. Sorting a 2D array based on the length of its subarrays.
 
 // Write a function that sorts an array of arrays (two-dimensional array)
 // based on the lengths of the subarrays (in ascending order).
 
-// ex. builtins.sort([[1, 2, 3], [4, 5], [6]]) -> [[6], [4, 5], [1, 2, 3]]
-// ex. builtins.sort([[], [''], []]) -> [[], [], ['']]
+// ex. builtins.sortByLength([[1, 2, 3], [4, 5], [6]]) -> [[6], [4, 5], [1, 2, 3]]
+// ex. builtins.sortByLength([[], [''], []]) -> [[], [], ['']]
 
-builtins.sort = function(array) {
+// Hint: Use the same Array sort() function - but think about what you're
+// comparing this time!
+
+builtins.sortByLength = function(array) {
   // YOUR CODE HERE
 };
 
@@ -122,8 +159,8 @@ builtins.sort = function(array) {
 
 // ex. builtins.flatten([[1, 2, 3], [4, 5], [6]]) -> [1, 2, 3, 4, 5, 6]
 // ex. builtins.flatten([[], [''], []]) -> ['']
+// ex. builtins.flatten([]) -> []
 
 builtins.flatten = function(array) {
   // YOUR CODE HERE
 };
-

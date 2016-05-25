1f2e2d051a8fbb684b635d7047503698af493746
Ethan Lee <ethanlee@ethansites.com>
2016-05-25 16:32:45 -0700

    Refactored calculator exercise flow, addition/subtraction-only

--- day1/util_calc.js
+++ day1/util_calc.js
@@ -1,23 +1,39 @@
 window.util = {};
 
-// Bonus Exercise! Write a function calc() that takes a string
-// that represents an arithmetic operation (such as "3 + 2") and
+// Calculator Exercise!
+
+// Write a function calc() that takes a string that
+// represents an arithmetic operation (such as "3 + 2") and
 // returns the numerical result of the operation.
 
-// Essentially, we are rewriting 
+// We'll start with addition and subtraction only!
 
-// We'll take this step-by-step.
+// ex. util.calc('3 + 2') -> 5
+// ex. util.calc('3 + 8 + 2 + 1    ') -> 14
+// ex. util.calc('2 - 1 + 5 + 6') -> 12
+// ex. util.calc('    -1 + 3 - 2 + 5') -> 5
 
 util.calc = function(op) {
-  // 1. See util.stripWhitespace()!
-  op = util.stripWhitespace(op);
+  // 1. First, using a builtin function you wrote before,
+  // trim any whitespace before and after the op string.
 
-  // 2. See util.explode()!
-  var parsed = util.explode(op);
-  var nums = parsed[0];
-  var ops = parsed[1];
+  // YOUR CODE HERE
 
-  // 3. See util.calcOne()! 
+  // 2. See util.splitIntoParts()!
+  var tokens = util.splitIntoParts(op);
+
+  // 3. See util.validateExpression()!
+  try {
+    util.validateExpression(tokens[0], tokens[1]);
+  }
+  catch(e) {
+    throw e;
+  }
+
+  var nums = tokens[0];
+  var ops = tokens[1];
+
+  // 4. See util.calcOne()! 
 
   // Hint: you'll be using calcOne() multiple times.
   // Use the loop below and for each calcOne() call,
@@ -28,50 +44,51 @@ util.calc = function(op) {
   // we'll be jumping into precedence with multiplication
   // and division next!
 
-  while(ops.length > 0) {
+  while (ops.length > 0) {
     // YOUR CODE HERE
-    var result = util.calcOne(nums[0], nums[1], ops[i]);
-
-    nums = nums.slice(1);
-    ops = ops.slice(1);
-
-    nums[0] = result;
   }
 
+  return result;
 };
 
-util.stripWhitespace = function(s) {
 
-  // 1. First, use the builtins we created earlier to remove all
-  // whitespace from the string "op". Hint: use search() and a while 
-  // loop to find all occurrences of whitespace and remove!
-
-  // Double-hint: the condition for the while loop should using
-  // your search function!
+util.splitIntoParts = function(s) {
+  // 2. For this step, we want to take a whitespace-stripped string
+  // representing an operation that looks like "3 + 3" and turn it into
+  // a two-dimensional array: the first array with all the parsed
+  // numbers and the second array with all the operators as strings.
+  
+  // ex. util.splitIntoParts('3 + 2') -> [[3, 2], ['+']]
+  // ex. util.splitIntoParts('3 + 8 + 2 + 1') -> [[3, 8, 2, 1], ['+', '+', '+']]
+  // ex. util.splitIntoParts('2 - 1 + 5 + 6') -> [[2, 1, 5, 6], ['-', '+', '+']]
+  // ex. util.splitIntoParts('-1 + 3 - 2 + 5') -> [[-1, 3, 2, 5], ['+', '-', '+']]
 
   // YOUR CODE HERE
-  while (builtins.search(s, " ")) {
-    s = s.substring(0, s.indexOf(" ")) + s.substring(s.indexOf(" ") + 1);
-  }
-  return s;
 };
 
-util.explode = function(s) {
-  // 2. For this step, we want to take a whitespace-stripped string
-  // representing an operation that looks like "3+3" and turn it into
-  // two arrays: one with the numbers between operators and one with
-  // the operators themselves.
+util.validateExpression = function(nums, ops) {
+  // 3. validateExpression() returns true if the operation resulting from 
+  // running our calc() function with the given nums and ops array is valid.
+  // Otherwise, it should throw an exception.
 
-  // ex. util.explode("3+5+6+7.1") -> [[3, 5, 6, 7.1], ["+", "+", "+"]]
+  // Write a function that determines that all the elements of the nums array
+  // are numbers and all of the characters of the ops array are valid
+  // arithmetic operators.
 
-  // To do this, we need to use regular expressions - TODO
+  // ex. util.validateExpression([3, 2], ['+']) -> true
+  // ex. util.validateExpression([3, 8, 2, 1], ['+', '+', '+']) -> true
+  // ex. util.validateExpression([2, 1, 5, 6], ['-', '+', '+']) -> true
+  // ex. util.validateExpression([-1, 3, 2, 5], ['+', '-', '+']) -> true
+  // ex. util.validateExpression([0, 3, 4], ['-', '+', '/']) -> Exception thrown: "Too many or too few operations"
+  // ex. util.validateExpression([], []) -> Exception thrown: "Too many or two few operations"
+  // ex. util.validateExpression([1], []) -> Exception thrown: "Too many or two few operations"
+  // ex. util.validateExpression(['+', '-', '*'], [1, 2]) -> Exception thrown: "Expected number, got +"
 
-  // YOUR CODE HERE 
-
-};
+  // YOUR CODE HERE
+}
 
 util.calcOne = function(a, b, op) {
-  // 3. calcOne() takes two numbers a and b and returns the numerical
+  // 4. calcOne() takes two numbers a and b and returns the numerical
   // result of the operation specified by op (could be "+", "-", "*", or "/").
 
   // ex. util.calcOne(3, 4, "+") -> 7
@@ -79,11 +96,4 @@ util.calcOne = function(a, b, op) {
   // ex. util.calcOne(5, 9, "-") -> -4
 
   // YOUR CODE HERE
-  return eval(a + op + b);
-
-  return {
-    "+": function (a, b) { return a + b; },
-    "-": (a, b) => a - b,
-    "*": (a, b) => 
-  }[op](a, b);
-}
+};

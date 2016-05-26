ad54b01452286d1eed6259d1527a12010aeea190
joshpaulchan <joshpaulchan@gmail.com>
2016-05-26 15:00:36 -0400

    Rearrange arguments and fix descriptions as per PR review

--- day2/stocks.js
+++ day2/stocks.js
@@ -11,27 +11,34 @@ window.stocks = {};
 // The 'time' property is the time of the transaction on 5/17/2016.
 // The 'price' property is the value of the transaction - can be negative.
 // You can access these properties the usual ways, like (assuming a given item is called `trans`): `var price = trans["price"];` or `var ticker = trans.ticker`
+// So, the data's gonna look something like:
+// var data = [ { "ticker": "MSFT", "time": "2016-05-17T11:02:20", "price": 22.83993924756409 }, ... ];
 
-// Exercise 1. stocks.biggestGainer(data<Transaction[]>)
+
+// Exercise 1. stocks.gainAndLoss(data<Transaction[]>)
+// Write a function that takes an array of Transaction objects. It should return an object with all the stocks as keys, and their values being an array with the first value being the total gain and the second being the total loss.
+// It should look like this: { "AMZN": [500, -25], ... }
+stocks.gainAndLoss = function(data) {
+  // YOUR CODE HERE
+};
+
+// Exercise 2. stocks.biggestGainer(data<Transaction[]>)
 // Write a function that takes an array of Transaction objects. It should return the ticker of the biggest gainer of the given list of stock transactions (the stock that made the most that day)
-//
-// hint. use maybe doing exercise 3 first will help.
+// 
+// ex. stocks.biggestGainer(stockData) -> 'AMZN'
+// 
+// hint. use maybe doing `gainAndLoss` first will help.
 stocks.biggestGainer = function(data) {
   // YOUR CODE HERE
 };
 
-// Exercise 2. stocks.biggestLoser(data<Transaction[]>)
+// Exercise 3. stocks.biggestLoser(data<Transaction[]>)
 // Write a function that takes an array of Transaction objects. It should return the ticker of the biggest loser of the stocks (the stock that lost the most that day)
 //
-// hint. maybe doing exercise 3 and 1 will help.
+// ex. stocks.biggestLoser(stockData) -> 'GOOG'
+// 
+// hint. maybe doing `gainAndLoss` and `biggestGainer` will help.
 stocks.biggestLoser = function(data) {
-  // YOUR CODE HERE
-};
-
-// Exercise 3. stocks.gainAndLoss(data<Transaction[]>)
-// Write a function that takes an array of Transaction objects. It should return an object with all the stocks as keys, and their values being an array with the first value being the total gain and the second being the total loss.
-// It should look like this: { "AMZN": [500, -25], ... }
-stocks.gainAndLoss = function(data) {
   // YOUR CODE HERE
 };
 
@@ -44,7 +51,7 @@ stocks.widestTradingRange = function(data) {
 };
 
 // [Bonus] Exercise 5. stocks.totalPortofolioValue(data<Transaction[]>, ticker<String>)
-// Write a function that takes an array of Transaction objects and a string corresponding to a ticker of a company in the transaction data. It should return the total portofolio value of the company you gave it.
+// Write a function that takes an array of Transaction objects and a string corresponding to a ticker of a company in the transaction data. It should return the total portofolio value of the given company at day's end.
 // 
 // hint. exercise 3 can help!
 stocks.totalPortofolioValue = function(data, ticker) {
@@ -52,7 +59,7 @@ stocks.totalPortofolioValue = function(data, ticker) {
 };
 
 // [Bonus] Exercise 6. stocks.totalPortofolioGains(data<Transaction[]>, ticker<String>)
-// Write a function that takes an array of Transaction objects and a string corresponding to a ticker of a company in the transaction data. It should return the total portofolio value of the company you gave it.
+// Write a function that takes an array of Transaction objects and a string corresponding to a ticker of a company in the transaction data. It should return the total portofolio value of the company at day's end.
 // 
 stocks.totalPortofolioGains = function(data, ticker) {
   // YOUR CODE HERE

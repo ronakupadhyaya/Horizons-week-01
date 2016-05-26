1907f1b15829933e6eaf11adcd8da21a0c626444
joshpaulchan <joshpaulchan@gmail.com>
2016-05-26 15:26:00 -0400

    Fix exercise descriptions and add new getGPA exercise'

--- day2/grades.js
+++ day2/grades.js
@@ -11,11 +11,28 @@ window.grades = {};
 // 'major', a string indicating the user's major (there are only 4 majors)
 // 'grades', an object with two keys, 'class1' and 'class2', whose values are integers from 1 to 4 indicating that students performance in the class.
 
-// [Helper] Exercise 0. grades.average(arr<Number[]>)
-// Write a function that takes an array of numbers and returns the sum of all of them.
+// [Helper] Exercise 0.A grades.average(arr<Number[]>)
+// Write a function that takes an array of numbers and returns the average of all of them.
 //
+// ex. grades.average([0, 1, 2, 3]) -> 1.5
+// ex. grades.average([1, 2, 4, 1]) -> 2
+// ex. grades.average([]) -> 0
+// ex. grades.average([0, 0]) -> 0
+// 
 // hint. use _.fold()
 grades.average = function(arr) {
+  // YOUR CODE HERE
+};
+
+// [Helper] Exercise 0.B grades.getGPA(student<Object>)
+// Write a function that takes an Student object and returns its GPA
+// note. remember that the student object has a .grades property, with two keys: 'class1' and 'class2'
+//
+// ex. grades.getGPA() -> 1.5
+// ex. grades.getGPA([0, 0]) -> 0
+// 
+// hint. use grades.average
+grades.getGPA = function(student) {
   // YOUR CODE HERE
 };
 
@@ -26,7 +43,7 @@ grades.highestGPA = function(data) {
   // YOUR CODE HERE
 }
 
-// Exercise 2. grades.highestGPA(data<Student[]>)
+// Exercise 2. grades.majorWithHighestGPA(data<Student[]>)
 // Write a function that takes an array of Student objects and returns the major of the student with the highest GPA
 //
 // hint. you can use highestGPA if you'd like.
@@ -34,11 +51,10 @@ grades.majorWithHighestGPA = function(data) {
   // YOUR CODE HERE
 };
 
-// Exercise 2. grades.highestGPA(data<Student[]>)
+// Exercise 3. grades.avgGPAPerClass(data<Student[]>)
 // Write a function that takes an array of Student objects and returns an object with two keys, `class1` and `class2`, with values that correspond to the average GPA of the students taking that class.
 // It should look like: { 'class1': 2, 'class2' : 2 }
 //
-// hint. you can use highestGPA if you'd like.
 grades.avgGPAPerClass = function(data) {
   // YOUR CODE HERE
 };

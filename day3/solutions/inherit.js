40aa23a2437049583b0448b3fd46c7647cccb67c
joshpaulchan <joshpaulchan@gmail.com>
2016-05-26 20:55:42 -0400

    Fix solutions according to PR notes

--- day3/solutions/inherit.js
+++ day3/solutions/inherit.js
@@ -8,7 +8,7 @@
 // Define one property on the class, `name`.
 // Also, define a *getter* (a method that get a value), getName() that return the `name` of the instance.
 
-// TODO: put your `Person` class code over here!
+// YOUR CODE HERE
 // Constructor & Properties
 
 var Person = function(name, age) {
@@ -27,33 +27,30 @@ Person.prototype.getName = function() {
 // The contructor for `Student` should take three arguments, just like before - `name`, `major` and `grades`.
 // note. "Inheritance" will allow the `Student` class to get all the properties and methods defined on the `Person` class while also allowing you to overload them.  This allows you to re-use code implemented elsewhere and provides a good way to  separate your logic.
 // 
-// ex.
-// 
 // hint. the syntax for defining a class that inherits from another (sometimes called a *sub-class* or *child-class*) is as such:
 // 
 // (`Fruit` is the parent class)
-//  var Fruit = function(color) {
-//    this.color = color;
-//  };
+var Fruit = function(color) {
+  this.color = color;
+};
+
+Fruit.prototype.getColor = funtion() {
+  return this.color;
+};
+
+var Banana = function() {
+  Fruit.call(this, 'yellow');
+};
 // 
-// Fruit.prototype.getColor = funtion() {
-//  return this.color;
-// };
-// 
-//  var Banana = function() {
-//    Fruit.call(this, 'yellow');
-//  };
-// 
-// Student.prototype = Object.create(Person.prototype) // pass the functions and properties from Fruit to Banana
-// Banana.prototype.constructor = Banana; // make the Banana its own man again
+Banana.prototype = new Fruit();  // pass the functions and properties from Fruit to Banana
 // 
 // Now, the strength (and beauty) of doing something like this is that you can now do:
-// var b = new Banana();
-// b.getColor() -> 'yellow'
+var b = new Banana();
+console.log("the banana is yellow: ", b.getColor() == 'yellow');
 // 
 // Even though getColor was not defined in the `Banana` class, it was defined on the `Fruit` class, which `Banana` inherits from, which it can then use.
 
-// TODO: put your constructor for student right here
+// YOUR CODE HERE
 
 var Student = function(name, major, grades) {
   Person.call(this, name);
@@ -62,8 +59,7 @@ var Student = function(name, major, grades) {
   this.grades = grades;
 };
 
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.construct = Student;
+Student.prototype = new Person();
 
 // Exercise 3. Method definitions
 // Write a `getIdentity` method for the `Student` class. This method should use the `getName` function and prefix the return value of that function with "Student - ".

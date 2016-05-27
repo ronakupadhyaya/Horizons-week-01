2172500bafc90e7bd3c41b55ed924edc840a8853
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-05-26 23:03:47 -0400

    Create queue files.

---
+++ day4/queue.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// XXX
+window.Queue = function() {
+  // YOUR CODE HERE
+}
+
+Queue.prototype.addToEnd = function(item) {
+  // YOUR CODE HERE
+}
+
+// Tests
+describe("Queue", function() {
+  // YOUR CODE HERE
+});

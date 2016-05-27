4aa3474a2015fea93c7e3394fc3ef3b906daf0b0
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-05-26 22:56:02 -0400

    wip

---
+++ day4/testing.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// We use Jasmine to do our testing
+// http://jasmine.github.io/2.4/introduction.html
+//
+// Tests are grouped in test "suites."
+//
+describe("", function() {
+  it("", function() {
+  });
+});

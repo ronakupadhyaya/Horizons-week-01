4bce713a5d8ecdd1fe50f45881587e8c959dcbff
joshpaulchan <joshpaulchan@gmail.com>
2016-05-28 19:32:55 -0400

    Remove implementations in

--- day3/dinosaur_game.js
+++ day3/dinosaur_game.js
@@ -49,21 +49,12 @@ game.Game.prototype = {
 		// hint. how do you iterate through an array and perform a function on each item?
 		// hint. use `.update()`!
 		// YOUR CODE HERE
-		this.player.update(this.tick);
-		this.obstacles.forEach(function(gObj) {
-			gObj.update(this.tick);
-		}, this);
 		
 		// Exercise 2.A Bound Player & Collision Detection | Player Care	
 		// Write a few lines that check if the player's position is under the canvas.The bottom border of the canvas is at position y =`this.height`, and the top level is y = 0. The player's position is accessible via `.getPosition()`
 		// If the player is under the canvas, stop the player from falling by setting its inAir property to false and setting it's position to the same x-point but vertically positioned ("stopped") the bottom border, `this.height`.
 		// 
 		// YOUR CODE HERE
-		var pPos = this.player.getPosition();
-		if (pPos[1] >= this.height) {
-			this.player.inAir = false;
-			this.player.setPosition(pPos[0], this.height);
-		}
 		
 		this.obstacles.forEach(function(gObj) {
 			// Exercise 2.B Bound Player & Collision Detection | Bound Obstacles
@@ -72,21 +63,13 @@ game.Game.prototype = {
 			// 
 			// hint. use `getPosition` and `setPosition`
 			// hint. the left border of the canvas is at x = 0;
-			var objPos = gObj.getPosition();
-			if (objPos[0] < 0) {
-				gObj.setPosition(this.width + gObj.width, objPos[1]);
-			}
+			// YOUR CODE HERE
 			
 			// Exercise 2.C Bound Player & Collision Detection | Check Collisions
 			// Write a block of code that calls `this.exit` if an obstacle has collided with the player object.
 			// 
 			// hint. use `gObj.isCollidingWith` but you'll need to implement it first! Check out `dinosaur.js`.
 			// YOUR CODE HERE
-			// check if hit player
-			if (gObj.isCollidingWith(this.player)) {
-				// end the game!
-				this.exit();
-			}
 			
 			// [helper] collideTime
 			// if collided and after 3 seconds, clear it

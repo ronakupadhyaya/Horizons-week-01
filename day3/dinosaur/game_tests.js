0805f71d9e8862d85f67fccb4106736c38a656fe
joshpaulchan <joshpaulchan@gmail.com>
2016-05-28 19:14:50 -0400

    Add tests for the game class

--- day3/dinosaur/game_tests.js
+++ day3/dinosaur/game_tests.js
@@ -4,12 +4,10 @@ describe("<Dinosaur> Class", function() {
   var dino;
   var ob1;
   var ob2;
-  var ob3;
   beforeAll(function() {
     dino = new game.Dinosaur(0, 0);
     ob1 = new game.Obstacle(20, 40);
     ob2 = new game.Obstacle(0, 40);
-    ob3 = new game.Obstacle(0, 0);
   })
   
   
@@ -19,37 +17,60 @@ describe("<Dinosaur> Class", function() {
     });
     
     it("should return a Boolean", function() {
-      expect(dino.isCollidingWith(dino, ob1)).toEqual(jasmine.any(Boolean));
+      expect(dino.isCollidingWith(ob1)).toEqual(jasmine.any(Boolean));
     });
     
     it("should properly detect collisions", function() {
-      expect(dino.isCollidingWith(dino, dino)).toEqual(true);
-      expect(dino.isCollidingWith(dino, ob1)).toEqual(false);
-      expect(dino.isCollidingWith(dino, ob2)).toEqual(false);
-      expect(dino.isCollidingWith(dino, ob3)).toEqual(true);
+      expect(dino.isCollidingWith(dino)).toEqual(true);
+      expect(dino.isCollidingWith(ob1)).toEqual(false);
+      expect(dino.isCollidingWith(ob2)).toEqual(false);
     });
   });
 });
 
 describe("<Game> Class", function() {
-  var game;
+  var g;
+  var timerCallback;
   beforeAll(function() {
-    game = new game.Game();
-  })
+    g = new game.Game();
+    
+    timerCallback = g.exit;
+    timerCallback = jasmine.createSpy("timerCallback");
+    jasmine.clock().install();
+  });
   
   it("the should update players and game objects", function() {
-    
+    expect(g.player.hasCalledUpdate).toBe(true);
+    g.obstacles.forEach(function(ob) {
+      expect(ob.hasCalledUpdate).toBe(true);
+    });
   });
   
   it("the should bound players", function() {
-
+    var dinoPos = g.player.getPosition();
+    expect(dinoPos[0]).toBeGreaterThan(-1);
+    expect(dinoPos[0]).toBeLessThan(g.width + 1);
+    expect(dinoPos[1]).toBeGreaterThan(-1);
+    expect(dinoPos[1]).toBeLessThan(g.height + 1);
   });
   
   it("the should bound obstacles", function() {
-    
+    g.obstacles.forEach(function(ob) {
+      var obPos = ob.getPosition();
+      expect(obPos[0]).toBeGreaterThan(-1);
+      expect(obPos[0]).toBeLessThan(g.width + 1);
+      expect(obPos[1]).toBeGreaterThan(-1);
+      expect(obPos[1]).toBeLessThan(g.height + 1);
+    });
   });
   
-  it("the should properly detect collisions", function() {
+  it("should properly call exit upon collision", function() {
+    setTimeout(function() {
+      timerCallback();
+    }, 1000);
+    
+    jasmine.clock().tick(1001);
+    expect(timerCallback).toHaveBeenCalled();
     
   });
 });

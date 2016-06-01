e92797fff5af8a70a55e8a079201354095bd2b3e
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-06-01 00:17:43 -0400

    Rename warmup and inline in solutions exercise.

--- day2/solutions/2_inline_truthiness.js
+++ day2/solutions/2_inline_truthiness.js
@@ -15,23 +15,6 @@ truthy.isTruthy = function(thing) {
   return !!thing;
 };
 
-// Exercise 2. truthy.isSortOfEqual(a, b)
-// Write a function that takes two arguments, `a` and `b`, and returns true or false when a and b are sort of the same.
-// ex. truthy.isSortOfEqual(1, 1) -> true
-// ex. truthy.isSortOfEqual(1, 0) -> false
-// ex. truthy.isSortOfEqual('1', 1) -> true
-// ex. truthy.isSortOfEqual('', ' ') -> false
-// ex. truthy.isSortOfEqual([ 'a', 'b' ], 'a') -> false
-// ex. truthy.isSortOfEqual([ 'a', 'b' ], { 0: 'a', 1: 'b'}) -> false
-// ex. truthy.isSortOfEqual({ 0: 'a', 1: 'b'}, { 0: 'a', 1: 'b'}) -> false
-///
-// hint. use `==` to compare
-// hint. see: http://stackoverflow.com/questions/359494/does-it-matter-which-equals-operator-vs-i-use-in-javascript-comparisons
-truthy.isSortOfEqual = function(a, b) {
-  // YOUR CODE HERE
-  return a == b;
-};
-
 // Exercise 3. truthy.max(a, b)
 // Write a function that takes two arguments, `a` and `b`, and returns the argument that has the highest value. If they both have the same value, return the first argument.
 // ex. truthy.max(1, 1) -> 1
@@ -83,19 +66,4 @@ truthy.or = function(a, b) {
   //   return false;
   // }
   return (a + b) ? true : false;
-};
-
-
-// Exercise 5. truthy.isReallyEquals(a<number>, b<number>)
-// Write a function that takes two strings as arguments, `a` and `b`, and returns true or false when a and b are equal.
-// Write a function that takes two numbers as arguments, `a` and `b`, and returns true or false when a and b are equal.
-// ex. truthy.compareStr('john', 'John') -> false
-// ex. truthy.compareStr('xDvz7h$k', 'xDvz7h$k') -> true
-// ex. truthy.isReallyEquals(1, 1) -> true
-// ex. truthy.isReallyEquals(1, 0) -> false
-// ex. truthy.isReallyEquals(1, 1.000000000000000000000000000000001) -> true
-//
-truthy.isReallyEquals = function(a, b) {
-  // YOUR CODE HERE
-  return a === b;
 };

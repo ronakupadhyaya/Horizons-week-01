7b9862f174c2579965daec36451532f99bdf6203
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-06-01 17:27:29 -0400

    Test cases for partial.

---
+++ challenges/underscore_extra.js
@@ -0,0 +1,12 @@
+// Week 1 Challenge Problem
+
+// function allArgs() {
+//  return _.toArray(arguments);
+// }
+// ex. partial(allArgs)() -> []
+// ex. partial(allArgs)(0, 1, 2, 3) -> [0, 1, 2, 3, 4]
+// ex. partial(allArgs, 'x')() -> ['x']
+// ex. partial(allArgs, 'x')(0, 1, 2, 3) -> ['x', 0, 1, 2, 3, 4]
+// ex. partial(allArgs, 'x')(0, 1, 2, 3) -> ['x', 0, 1, 2, 3, 4]
+function partial() {
+}
---
+++ challenges/underscore_extra_tests.js
@@ -0,0 +1,33 @@
+
+describe('partial()', function() {
+  function allArgs() {
+    return _.toArray(arguments);
+  }
+
+  it("partial(allArgs)() -> []", function() {
+    expect(partial(allArgs)() ).toEqual([]);
+  });
+  it("partial(allArgs)(0, 1, 2, 3) -> [0, 1, 2, 3, 4]", function() {
+    expect(partial(allArgs)(0, 1, 2, 3) ).toEqual([0, 1, 2, 3, 4]);
+  });
+  it("partial(allArgs, 'x')() -> ['x']", function() {
+    expect(partial(allArgs, 'x')() ).toEqual(['x']);
+  });
+  it("partial(allArgs, 'x')(0, 1, 2, 3) -> ['x', 0, 1, 2, 3, 4]", function() {
+    expect(partial(allArgs, 'x')(0, 1, 2, 3) ).toEqual(['x', 0, 1, 2, 3, 4]);
+  });
+  it("partial(allArgs, 'x')(0, 1, 2, 3) -> ['x', 0, 1, 2, 3, 4]", function() {
+    expect(partial(allArgs, 'x')(0, 1, 2, 3) ).toEqual(['x', 0, 1, 2, 3, 4]);
+  });
+  it("handle 500 arguments from partial", function() {
+    var args = [allArgs].concat(_.range(500));
+    expect(partial.apply(null, args)()).toEqual(_.range(500));
+  });
+  it("handle 500 arguments after partial", function() {
+    expect(partial(allArgs).apply(null, _.range(500))).toEqual(_.range(500));
+  });
+  it("handle 500 arguments from and after partial", function() {
+    var args = [allArgs].concat(_.range(500));
+    expect(partial.apply(null, args).apply(null, _.range(500))).toEqual(_.range(500).concat(_.range(500)));
+  });
+});

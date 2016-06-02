8d3ea08fde2a0dc5e503ad7123a2905176bb134a
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-06-01 22:19:54 -0400

    Edit distance.

---
+++ challenges/edit_distance.js
@@ -0,0 +1,16 @@
+// Challenge: Edit distance
+//
+// Given two strings x and y, calculate the edit distance of x and y, i. e. the
+// minimum number of edits needed to transform x into y. The allowed edits are:
+// - insert a single item
+// - delete a single item
+// - replace a single item with another item
+//
+// Edit distance is otherwise known as Levenshtein distance.
+// https://en.wikipedia.org/wiki/Levenshtein_distance
+//
+// Adapted from 4clojure
+// https://www.4clojure.com/problem/101
+function editDistance() {
+  // YOUR CODE HERE
+}

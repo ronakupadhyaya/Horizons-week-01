9f972e1452451a29e6aada189a37d22ba246af3e
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-12-07 11:33:59 -0800

    w1d4 dino: new starting point code

--- day4/dinosaur.js
+++ day4/dinosaur.js
@@ -1,9 +1,9 @@
 "use strict";
 
-window.game = window.game || {}
-
-// `Dinosaur` Class
-// YOUR CODE HERE
-
-// `Obstacle` Class
-// YOUR CODE HERE
+// Wait till the game ready to run
+game.onReady(function() {
+  // Sample code to clear the screen then draw an obstacle.
+  // Feel free to delete all this.
+  game.clear();
+  game.drawObstacle(100, 100);
+});

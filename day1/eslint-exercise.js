0d070d2c90f3979dbf3f23b3125c80f05508c2d2
Prathmesh Desai <d.prathmesh@gmail.com>
2017-01-12 11:41:33 -0800

    create linter exercise

---
+++ day1/eslint-exercise.js
@@ -0,0 +1,18 @@
+/**
+ * This document has common linting errors that you may come across. Your job is to go through the document
+ * and fix them.
+**/
+
+function wrongIndent() {
+  if (true == true)
+    {
+     return 'Indentation must always be 2 spaces';;
+  }
+}
+
+  var Student = {
+    "name": 'Paul',
+    "age": 8
+    ,
+    "occupation": 'Vegan',
+  }

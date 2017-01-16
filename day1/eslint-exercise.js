71dfdac8e07b5a6332432f18916ab5a18fd5de29
Yasmeen Wermers <yasmeen.wermers@emory.edu>
2017-01-16 14:50:46 -0800

    initial changes and git/rocketship exercise

--- day1/eslint-exercise.js
+++ day1/eslint-exercise.js
@@ -1,18 +1,17 @@
 /**
- * This document has common linting errors that you may come across. Your job is to go through the document
+ * This document has common linting errors that you may come across.
+ Your job is to go through the document
  * and fix them.
 **/
 
 function wrongIndent() {
-  if (true == true)
-    {
-     return 'Indentation must always be 2 spaces';;
+  if(true){
+    return 'Indentation must always be 2 spaces';
   }
 }
 
-  var Student = {
-    "name": 'Paul',
-    "age": 8
-    ,
-    "occupation": 'Vegan',
-  }
+var Student = {
+  "name": 'Paul',
+  "age": 8,
+  "occupation": 'Vegan',
+}

61abc567a8f051792eb2b1690a968c0f13a5a217
Moose Paksoy <mustafa@joinhorizons.com>
2017-01-15 18:52:21 -0800

    tmp

--- day4/underscore_extra.js
+++ day4/underscore_extra.js
@@ -1,20 +1,48 @@
-// Week 1 challenge: more functions from underscore.
+"use strict";
+
+// Underscore function functions (aka Functional Underscore)
 //
 // Implement the following functions from the underscore library.
 // You'll need to use the 'arguments' object and function.apply()
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
 
-// http://underscorejs.org/#partial
-function partial() {
-  // YOUR CODE HERE
-}
+// Add _.once()
 
-// http://underscorejs.org/#compose
-function compose() {
-  // YOUR CODE HERE
-}
-
+// Write a function called 'memoize' that takes a function 'fn' and returns
+// a new function 'memoizedFn' that caches previously computed return values.
+//
+// When 'memoizedFn' is called with a new argument/parameter value, call
+// through to 'fn' and save the result of that call. If 'memoizedFn' is
+// called again with the same argument, return the previously saved
+// value without calling 'fn'.
+//
+// You can assume fn takes only a single argument and that this argument is a
+// number.
+//
+// function double(number) {
+//   console.log('called');
+//   return number * 2;
+// }
+// var memoizedDouble = memoize(double);
+// memoizedDouble(1) -> returns 2, prints 'called'
+// memoizedDouble(1) -> returns 2
+// memoizedDouble(2) -> returns 4, prints 'called'
+// memoizedDouble(2) -> returns 4
+//
+// This is a simplified version of _.memoize() without hashFunction
 // http://underscorejs.org/#memoize
 function memoize(func) {
   // YOUR CODE HERE
 };
+
+// Implement _.partial()
+// http://underscorejs.org/#partial
+function partial(fn) {
+  // YOUR CODE HERE
+}
+
+// Bonus: Implement _.compose()
+// http://underscorejs.org/#compose
+function compose() {
+  // YOUR CODE HERE
+}
--- day4/underscore_extra_tests.js
+++ day4/underscore_extra_tests.js
@@ -36,27 +36,74 @@ describe('partial()', function() {
   });
 });
 
-
 describe('compose()', function() {
-  it("f(g(h(x, y, z)))", function() {
+  it("compose(f, g, h) -> f(g(h(x, y, z)))", function() {
     function h(x, y, z) {
       expect(arguments.length).toBe(3);
-      return z * y;
+      return x * z * y;
     }
     function g(x) {
       expect(arguments.length).toBe(1);
-      return x;
+      return x / 3;
     }
     function f(x) {
       expect(arguments.length).toBe(1);
       return x * 2;
     }
     var composed = compose(f, g, h);
-    expect(composed(1, 2, 3)).toBe(12);
+    expect(composed(2, 3, 5)).toBe(20);
   });
 });
 
 describe("memoize()", function() {
+  var called, memoizedDouble;
+  function double(number) {
+    called++;
+    return number * 2;
+  }
+
+  beforeEach(function() {
+    called = 0;
+    memoizedDouble = memoize(double);
+  });
+
+
+  it('should return a function', function() {
+    expect(memoizedDouble).toEqual(jasmine.any(Function));
+  });
+
+  it('if memoizedDouble() is not called, then double() is not called', function() {
+    expect(called).toBe(0);
+  });
+
+  it('if memoizedDouble(1) is called once, then double(1) is called once and returns 2', function() {
+    expect(memoizedDouble(1)).toBe(2);
+    expect(called).toBe(1);
+  });
+
+  it('if memoizedDouble(1) is called 3 times, then double(1) is called once and returns 2', function() {
+    for (var i = 0; i < 3; i++) {
+      expect(memoizedDouble(1)).toBe(2);
+    }
+    expect(called).toBe(1);
+  });
+
+  it('if memoizedDouble() is called once with 1, 5, 10, then double() is called once with 1, 5, 10 and returns 2, 10 and 20 respectively', function() {
+    expect(memoizedDouble(1)).toBe(2);
+    expect(memoizedDouble(5)).toBe(10);
+    expect(memoizedDouble(10)).toBe(20);
+    expect(called).toBe(3);
+  });
+
+  it('if memoizedDouble() is called 3 times with 1, 5, 10, then double() is called once with 1, 5, 10 and returns 2, 10 and 20 respectively', function() {
+    for (var i = 0; i < 3; i++) {
+      expect(memoizedDouble(1)).toBe(2);
+      expect(memoizedDouble(5)).toBe(10);
+      expect(memoizedDouble(10)).toBe(20);
+    }
+    expect(called).toBe(3);
+  });
+
   it("calling 10 times with same 100 arguments calls only once", function() {
     var called = [];
     var fun = memoize(function(i) {

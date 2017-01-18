af99a0547d3a0d082aac11e84f5c826c7683aad8
Syed Mohsin <syedm.90@gmail.com>
2017-01-17 19:12:41 -0800

    copied over file manually from rajiv to avoid merge conflict

--- day2/dates.js
+++ day2/dates.js
@@ -11,6 +11,7 @@ window.dates = {};
 // hint. see http://www.w3schools.com/js/js_dates.asp
 dates.createDate = function(dateStr) {
   // YOUR CODE HERE
+  return new Date(dateStr);
 };
 
 // Exercise 2. dates.getUTCString(dateObj<Date>)
@@ -24,6 +25,7 @@ dates.createDate = function(dateStr) {
 // hint. see http://www.w3schools.com/js/js_dates.asp
 dates.getUTCString = function(dateObj) {
   // YOUR CODE HERE
+  return dateObj.toUTCString();
 };
 
 // Exercise 3.A dates.isSameDayOfWeek(dateObj<Date>, otherDateObj<Date>)
@@ -35,6 +37,10 @@ dates.getUTCString = function(dateObj) {
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
 dates.isSameDayOfWeek = function(dateObj, otherDateObj) {
   // YOUR CODE HERE
+  if(dateObj.getDay() === otherDateObj.getDay()){
+    return true;
+  }
+  return false;
 };
 
 // Exercise 3.B dates.isSameTimeOfDay(dateObj<Date>, otherDateObj<Date>)
@@ -48,6 +54,10 @@ dates.isSameDayOfWeek = function(dateObj, otherDateObj) {
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getHours
 dates.isSameTimeOfDay = function(dateObj, otherDateObj) {
   // YOUR CODE HERE
+  if(dateObj.getHours() === otherDateObj.getHours() && dateObj.getMinutes() === otherDateObj.getMinutes() && dateObj.getSeconds() === otherDateObj.getSeconds()){
+    return true;
+  }
+  return false;
 };
 
 // Exercise 3.C dates.isTheFuture(dateObj<Date>)
@@ -56,6 +66,8 @@ dates.isSameTimeOfDay = function(dateObj, otherDateObj) {
 // hint. how do you check if something is 'bigger than' something else?
 dates.isTheFuture = function(dateObj) {
   // YOUR CODE HERE
+  var curDate = new Date();
+  return curDate < dateObj;
 };
 
 // Exercise 4. dates.incrementDay(dateObj<Date>)
@@ -66,4 +78,6 @@ dates.isTheFuture = function(dateObj) {
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setDate
 dates.incrementDay = function(dateObj) {
   // YOUR CODE HERE
+  dateObj.setDate(dateObj.getDate() + 1);
+  return dateObj;
 };

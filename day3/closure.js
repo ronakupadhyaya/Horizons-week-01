27b8f160e0988374fad0e5c208b17a8337341251
lisaztruong <lisaztruong@gmail.com>
2017-01-18 13:58:55 -0800

    working on day3

--- day3/closure.js
+++ day3/closure.js
@@ -15,6 +15,9 @@
 // this function is to hide the password from prying eyes.
 function vault(password) {
   // YOUR CODE HERE
+  return function (attempt){
+    return attempt===password;
+  }
 }
 
 // This function returns an object that leaks private information!
@@ -25,10 +28,7 @@ var createUser = function(username, password) {
     // Delete privatePassword and use vault()
     // to implement the login function
     // YOUR CODE HERE
-    privatePassword: password,
-    login: function(attempt) {
-      return this.privatePassword === attempt;
-    }
+    login: vault(password)
   }
 }
 
@@ -83,10 +83,20 @@ var horizons = createUser('horizons', 'horizonites');
 // ex. exponentiateNum(6, 5) -> 3125
 var once = function(f) {
   var called = false; // Let's create a local variable to track if f has been called
+  var used;
   return function() {
+    var argLength = arguments.length;
+    // first time it's called
     if (! called) { // if f hasn't been called yet
       f(); // call f
+      used = arguments;
+      // bind to object (this)
+      f.apply(this, arguments);
       called = true; // mark f as called
+    // after first time it's called
+    } else {
+      f(); // call f
+      f.apply(this, used);
     }
   }
 }

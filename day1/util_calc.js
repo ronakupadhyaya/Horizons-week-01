28ebc5f1b1a010e534588769053ec70bb090c456
AMY JIN <zjin@wellesley.edu>
2017-05-29 18:04:33 -0700

    exercise 4 and 5 by Hennessey333 and amyzjin

--- day1/util_calc.js
+++ day1/util_calc.js
@@ -19,7 +19,176 @@ window.util = {};
 // ex. util.calc('1 + 55 -2') -> Error, too many numbers
 // ex. util.calc('29 + + 1') -> Error, too many operators
 // ex. util.calc('29 + 1 +') -> Error, too many operators
-//
+util.isNumberTF = function(expression){
+  expression2 = expression.split(' ')
+  var y=false
+
+  for (var i=0;i<expression2.length;i++){
+    var index=expression2[i].length-1;
+    var index2=expression2[i][index];
+
+      if (index2=='0'){
+      y=true;
+    }
+    if (index2=='1'){
+      y=true;
+    }
+    if (index2=='2'){
+      y=true;
+    }
+    if (index2=='3'){
+      y=true;
+    }
+    if (index2=='4'){
+      y=true;
+    }
+    if (index2=='5'){
+      y=true;
+    }
+    if (index2=='6'){
+      y=true;
+    }
+    if (index2=='7'){
+      y=true;
+    }
+    if (index2=='8'){
+      y=true;
+    }
+    if (index2=='9'){
+      y=true;
+    }
+  }
+  return y;
+}
+util.isNumber = function(expression){
+  expression2 = expression.split(' ')
+  var y=0
+
+  for (var i=0;i<expression2.length;i++){
+    var index=expression2[i].length-1;
+    var index2=expression2[i][index];
+
+      if (index2=='0'){
+      y+=1;
+    }
+    if (index2=='1'){
+        y+=1;
+    }
+    if (index2=='2'){
+        y+=1;
+    }
+    if (index2=='3'){
+        y+=1;
+    }
+    if (index2=='4'){
+        y+=1;
+    }
+    if (index2=='5'){
+        y+=1;
+    }
+    if (index2=='6'){
+        y+=1;
+    }
+    if (index2=='7'){
+        y+=1;
+    }
+    if (index2=='8'){
+        y+=1;
+    }
+    if (index2=='9'){
+        y+=1;
+    }
+  }
+  return y
+}
+
+util.isOperator=function(expression){
+  expression2 = expression.split(' ')
+  var x=0
+  for (var i=0;i<expression2.length;i++){
+    var index=expression2[i].length-1;
+    var index2=expression2[i][index];
+    if (index2=='+'){
+      x+=1;
+    }
+    else if (index2=='-'){
+      x+=1;
+    }
+    else if (index2=='/'){
+      x+=1;
+    }
+    else if (index2=='*'){
+      x+=1;
+    }
+  }
+  return x
+}
+
+util.isPlus=function(expression){
+  expression2 = expression.split(' ')
+  for (var i=0;i<expression2.length;i++){
+    var index=expression2[i].length-1;
+    var index2=expression2[i][index];
+    if (index2=='+'){
+      return '+'
+    }
+}
+}
+
+
+
+
+util.calc = function(expression) {
+  var operators='+-/*'
+  // var x = 0
+  var y=0
+  var z =0
+  if (expression===''){
+    throw 'Error, empty expression'
+  }
+  if (util.isOperator(expression)===expression.length){
+    throw 'Error mission operator'
+  }
+
+    if (!util.isNumberTF(expression)){
+      throw 'Error no number'
+    }
+
+      if (expression[0]=='+'){
+        throw 'Error operator wrong spot'
+      }
+      // else if (expression[0]=='-'){
+      //   throw 'Error operator wrong spot'
+      // }
+      else if (expression[0]=='/'){
+        throw 'Error operator wrong spot'
+      }
+      else if (expression[0]=='*'){
+        throw 'Error operator wrong spot'
+      }
+
+
+
+    for (var i=0;i<expression.length;i++){
+
+      if (util.isNumber(expression)!==(util.isOperator(expression)+1)){
+        throw 'Error too many numbers/operators'
+    }
+  }
+
+
+    // for (var i=0;i<expression.length;i++){
+    //
+    //   var b=expression[i];
+    //   if (util.isNumberTF(b)){
+    //     expression[i] = (parseInt(b));
+    //   }
+    // }
+    // console.log(expression);
+    //
+    return eval(expression)
+
+  };
 // Part 2. Implement support for addition and subtraction.
 //
 // ex. util.calc('1') -> 1
@@ -53,6 +222,3 @@ window.util = {};
 // ex. util.calc('-1 * sqrt 4 - 3') -> -5
 // ex. util.calc('sqrt 9 - 3 * 10') -> -27
 // ex. util.calc('10 * sqrt 81') -> 90
-util.calc = function(expression) {
-  // YOUR CODE HERE
-};

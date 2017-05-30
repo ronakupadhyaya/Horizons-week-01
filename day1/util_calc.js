ed6d938c9460cc90db2accb1c78bf3dc052050a1
gsmith98 <smitgw11@gmail.com>
2017-05-29 21:05:21 -0700

    completed part 3 of codealong

--- day1/util_calc.js
+++ day1/util_calc.js
@@ -97,6 +97,60 @@ function plusMinus(expression) {
 // ex. util.calc('1 / 0 + 1 * 0') -> Infinity
 // ex. util.calc('1 / 0 * 0 + 1') -> NaN
 //
+
+//"1 + 2 - 3"
+//"1 + 3 * 4 - 2" ---> "1 + 12 - 2" ---> "11"
+//"1 + 3 * 4 - 2 / 2" ---> "1 + 12 - 1" ---> "12"
+//"1 + 3 * 4 / 2 - 2" --->  1 + 12 / 2 - 2  ----> "1 + 6 - 2" ---> "5"
+function multDiv(expression) {
+  // FIRST ATTEMPT!
+  // RAN INTO AN ISSUE WITH CHAINING (and more)
+  //var tokens = expression.split(' ');
+
+  // var result = "";
+  // for ( var i = 1; i < tokens.length; i+=2) {
+  //   if ( tokens[i] === '+' || tokens[i] === '-') {
+  //     result += tokens[i-1] + ' ' + tokens[i] + ' ';
+  //
+  //     // check if next character is the final character
+  //     // this is because we want to add the last one
+  //     if (i+1 === tokens.length-1) {
+  //       result += tokens[i+1];
+  //     }
+  //   } else if (tokens[i] === '*') {
+  //     result += tokens[i-1] * tokens[i+1];
+  //   } else if (tokens[i] === '/') {
+  //     result += tokens[i-1] / tokens[i+1];
+  //   }
+  // }
+
+  var tokens = expression.split(' ');
+
+  var i = 1;
+  while(i < tokens.length) {
+    if ( tokens[i] === '+' || tokens[i] === '-') {
+      i += 2;
+    } else if (tokens[i] === '*') {
+      var prod = tokens[i-1] * tokens[i+1]
+
+      // i = 1
+      // 3 * 2 + 1
+      // 6 + 1 * 9
+      // 6 + 9
+      tokens = tokens.slice(0, i-1).concat([prod], tokens.slice(i+2, tokens.length));
+
+    } else if (tokens[i] === '/') {
+      var quot = tokens[i-1] / tokens[i+1];
+
+      tokens = tokens.slice(0, i-1).concat([quot], tokens.slice(i+2, tokens.length));
+    }
+  }
+  // turn tokens into a string
+
+  return tokens.join(' ');
+}
+
+
 // Bonus: Implement support for the square root operator.
 // Implement support for the `sqrt` operator. `sqrt` is an operator that takes
 // only one argument (i.e. a unary operator). `sqrt` applied before all other
@@ -111,8 +165,11 @@ function plusMinus(expression) {
 // ex. util.calc('-1 * sqrt 4 - 3') -> -5
 // ex. util.calc('sqrt 9 - 3 * 10') -> -27
 // ex. util.calc('10 * sqrt 81') -> 90
+
+
+
 util.calc = function(expression) {
   checkValid(expression);
 
-  //
+  return plusMinus(multDiv(expression));
 };

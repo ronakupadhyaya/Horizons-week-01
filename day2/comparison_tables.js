574ae2c893b6a4489d235b012681ed1985143b0a
pradyutbansal <pradyutbansal@gmail.com>
2017-05-31 13:01:29 -0700

    inline activities

--- day2/comparison_tables.js
+++ day2/comparison_tables.js
@@ -6,13 +6,13 @@ window.comp = {};
 // for all different types - from booleans to numbers to arrays -
 
 var valuesToCheck = function() {
-  return [true, false, 1, 0, -1, "true", "false", "1", "0", 
-"-1", "", null, undefined, Infinity, -Infinity, [], {}, [[]], [0], [1], NaN];
+  return [true, false, 1, 0, -1, "true", "false", "1", "0",
+    "-1", "", null, undefined, Infinity, -Infinity, [], {}, [[]], [0], [1], NaN];
 }
 
 // and you will write the following:
 
-// 1. A function that evaluates the loosely equal (==) truth value of each value 
+// 1. A function that evaluates the loosely equal (==) truth value of each value
 // in valuesToCheck with every other value in valuesToCheck
 // 2. A function that evaluates the striclty equal (===) truth value of each value
 // in valuesToCheck with every other value in valuesToCheck
@@ -26,9 +26,9 @@ var valuesToCheck = function() {
 // ex. comp.testStrictEquality(s) ->
 // {"true_true": true, "true_false": false, "true_1": false, "true_0": false, ...}
 
-// Each property's key value should be formatted as: 
+// Each property's key value should be formatted as:
 // valuesToCheck[someIndex]_valuesToCheck[anotherIndex]
-// such that the return object has all keys of possible combinations of 
+// such that the return object has all keys of possible combinations of
 // valuesToCheck, from true_true to NaN_NaN.
 
 // Note: Allow for redundancies; you should have both true_false and false_true
@@ -46,10 +46,72 @@ var valuesToCheck = function() {
 
 // Good luck!
 
+comp.valToString = function(value) {
+  if (typeof value === "string") {
+  	return '"' + value + '"';
+  } else if (typeof value === "boolean") {
+  	return value + "";
+  } else if (typeof value === "number") {
+  	return value + "";
+  } else if (value === undefined) {
+  	return "undefined";
+  } else if (value === null) {
+  	return "null";
+  } else {
+  	if (value.length !== 0) {
+  		if (value[0] === 0) {
+  			return "[0]";
+  		} else if (value[0] === 1) {
+  			return "[1]";
+  		} else if (typeof value[0] === "object") {
+  			return "[[]]";
+  		} else {
+  			return "{}";
+  		}
+  	} else if (value + "" === ""){
+  			return "[]";
+  		}
+  		return "{}";
+
+  }
+};
+
 comp.testLooseEquality = function() {
-    // YOUR CODE HERE
+	//debugger;
+  var values = valuesToCheck();
+  var values2 = valuesToCheck();
+  var toReturn = {};
+  for (var i = 0; i < values.length; i++) {
+    	for (var j = 0; j < values.length; j++) {
+    		var firstVal = comp.valToString(values[i]);
+    		var secondVal = comp.valToString(values2[j]);
+    		var key = firstVal + '_' + secondVal;
+    		var boolVal = false;
+    		if (values[i] == values2[j]) {
+    			boolVal = true;
+    		}
+    		toReturn[key] = boolVal;
+    	}
+  }
+  return toReturn;
 };
 
 comp.testStrictEquality = function() {
-    // YOUR CODE HERE
+    //debugger;
+  var values = valuesToCheck();
+  var values2 = valuesToCheck();
+  var toReturn = {};
+  for (var i = 0; i < values.length; i++) {
+    	for (var j = 0; j < values.length; j++) {
+    		var firstVal = comp.valToString(values[i]);
+    		var secondVal = comp.valToString(values2[j]);
+    		var key = firstVal + '_' + secondVal;
+    		var boolVal = false;
+    		if (values[i] === values2[j]) {
+    			boolVal = true;
+    		}
+    		toReturn[key] = boolVal;
+    	}
+  }
+  return toReturn;
 };

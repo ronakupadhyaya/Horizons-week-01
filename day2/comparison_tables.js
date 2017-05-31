67d10092c68a094c3b03cef3053bb5e572bcafab
omeshomes <omid.rooholfada@yale.edu>
2017-05-31 13:22:10 -0700

    update from days 2/3

--- day2/comparison_tables.js
+++ day2/comparison_tables.js
@@ -46,10 +46,151 @@ var valuesToCheck = function() {
 
 // Good luck!
 
+// comp.truthSeeker = function(value) {
+// 	if (value) {
+// 		return true
+// 	}
+// 	else {
+// 		return false
+// 	}
+// }
+
 comp.testLooseEquality = function() {
-    // YOUR CODE HERE
+  var myObj = {}
+  var values = valuesToCheck();
+  for (var i = 0; i < values.length; i++) {
+    for (var j = 0; j < values.length; j++) {
+      // console.log("i", values[i]);
+      // console.log("j", values[j]);
+      if (typeof(values[i]) === 'string') {
+        var first = '\"' + values[i] + '\"' 
+      } else if (values[i] instanceof Object) {
+        var temp = values[i];
+        first = JSON.stringify(values[i]);
+        values[i] = temp;
+      } else {
+        first = values[i]
+      }
+      if (typeof(values[j]) === 'string') {
+        var second = '\"' + values[j] + '\"' 
+      } else if (values[j] instanceof Object) {
+        var temp = values[j];
+        second = JSON.stringify(values[j])
+        values[j] = temp;
+      } else {
+        second = values[j];
+      }
+      if (values[i] instanceof Object && values[j] instanceof Object) {
+        myObj[first + "_" + second] = false;
+      } else {
+        if (values[i] == values[j]) {
+          myObj[first + "_" + second] = true;
+        } else {
+          myObj[first + "_" + second] = false;
+        }
+      }
+    }
+  }
+  //console.log(myObj);
+  return myObj;
 };
 
+
+
+
+    // var perm = []
+    // var values = valuesToCheck()
+    // for (var i = 0; i < valuesToCheck().length; i++) {
+    // 	for (var j = 0; j < valuesToCheck().length; j++) {
+
+    // 		perm.push([valuesToCheck()[i], valuesToCheck()[j]])
+    // 	}
+    // }
+    // //console.log(perm)
+    // var final = {}
+    // var temp = true
+    // var temp1
+    // var temp2
+    // for (var k = 0; k < perm.length; k++) {
+    // 	if (typeof perm[k][0] === "string") {
+    // 		temp1 = '"' + perm[k][0] + '"'
+    // 	}
+    // 	else if (Array.isArray(perm[k][0])) {
+    // 		temp1 = '[]'
+    // 		console.log(temp1)
+    // 	}
+    // 	else if (typeof perm[k][0] === 'object' && !perm[k][0]) {
+    // 		temp1 = null
+    // 	}
+    // 	else if (typeof perm[k][0] === 'object') {
+    // 		temp1 = '{}'
+    // 	}
+    // 	else {
+    // 		temp1 = perm[k][0]
+    // 	}
+    // 	//repeat for temp2
+    // 	if (typeof perm[k][1] === "string") {
+    // 		temp2 = '"' + perm[k][1] + '"'
+    // 	}
+    // 	else if (Array.isArray(perm[k][1])) {
+    // 		temp2 = '[]'
+    // 	}
+    // 	else if (typeof perm[k][1] === 'object' && !perm[k][1]) {
+    // 		temp2 = null
+    // 	}
+    // 	else if (typeof perm[k][1] === 'object') {
+    // 		temp2 = '{}'
+    // 	}
+    // 	else {
+    // 		temp2 = perm[k][1]
+    // 		console.log(temp2)
+    // 	}
+    // 	//Object.assign(final, { (temp1 + "_" + temp2) : (perm[k][0] == perm[k][1])})
+    // 	final[temp1 + "_" + temp2] = (perm[k][0] == perm[k][1])
+    // 	//console.log(temp1 + "_" + temp2)
+
+    // 	//console.log(k, (perm[k][0] == perm[k][1]))
+    // }
+
+    // console.log(final)
+    // return final
+
 comp.testStrictEquality = function() {
-    // YOUR CODE HERE
+      var myObj = {}
+  var values = valuesToCheck();
+  for (var i = 0; i < values.length; i++) {
+    for (var j = 0; j < values.length; j++) {
+      // console.log("i", values[i]);
+      // console.log("j", values[j]);
+      if (typeof(values[i]) === 'string') {
+        var first = '\"' + values[i] + '\"' 
+      } else if (values[i] instanceof Object) {
+        var temp = values[i];
+        first = JSON.stringify(values[i]);
+        values[i] = temp;
+      } else {
+        first = values[i]
+      }
+      if (typeof(values[j]) === 'string') {
+        var second = '\"' + values[j] + '\"' 
+      } else if (values[j] instanceof Object) {
+        var temp = values[j];
+        second = JSON.stringify(values[j])
+        values[j] = temp;
+      } else {
+        second = values[j];
+      }
+      if (values[i] instanceof Object && values[j] instanceof Object) {
+        myObj[first + "_" + second] = false;
+      } else {
+        if (values[i] === values[j]) {
+          myObj[first + "_" + second] = true;
+        } else {
+          myObj[first + "_" + second] = false;
+        }
+      }
+    }
+  }
+  //console.log(myObj);
+  return myObj;
 };
--- day3/this.js
+++ day3/this.js
@@ -17,12 +17,12 @@ function getThis() {
 dis.getThis = getThis;
 
 dis.simpleGetThis = function() {
-  return dis.getThis() === null /* YOUR CODE HERE */;
+  return dis.getThis() === dis;
 };
 
 dis.assignGetThis = function() {
   dis.object2.getThis = getThis;
-  return dis.object2.getThis() === null /* YOUR CODE HERE */;
+  return dis.object2.getThis() === object2;
 };
 
 dis.callGetThis = function() {

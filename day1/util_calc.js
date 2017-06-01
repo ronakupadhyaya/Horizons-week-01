8ec8b65aadad780105386c3ba03b6fff1788fa2a
Julia <jklugher@wellesley.edu>
2017-05-31 17:57:03 -0700

    Add all day1 files

--- day1/util_calc.js
+++ day1/util_calc.js
@@ -11,39 +11,6 @@ window.util = {};
 //
 // Part 1. If an invalid expression is given, throw an exception.
 //
-
-function checkValid(toCheck) {
-
-  // a string is valid if:
-
-  // space-sperated
-  // alternates numbers and operators
-  // begins and ends with a number
-
-  var tokens = toCheck.split(' ');
-  // if the arugments is not odd than it is not valid at all so we don't b
-  //other checking it any further"
-  if (tokens.length % 2 === 0) {
-    throw "Expected an odd number of arguments, instead of even";
-  }
-
-  for (var i = 0; i < tokens.length; i++) {
-
-    if (i % 2 === 0) { //for even index
-      // this should be a number
-      if (isNaN(parseFloat(tokens[i]))) { //dont even need parseFloat because isNaN("4.5") => false
-        throw "Expected a number, found " + tokens[i] + " at position at " + i;
-      }
-    } else {
-    // this should be an operator
-      if (!(tokens[i] === "+" || tokens[i] === "-" || tokens[i] === "*" || tokens[i] === "/")) {
-        throw "Expected an operator, found " + tokens[i] + " at position at " + i;
-      }
-    }
-  }
-
-}
-
 // ex. util.calc('') -> Error, empty expression
 // ex. util.calc('1 2') -> Error, missing operator
 // ex. util.calc('-') -> Error, no numbers
@@ -54,21 +21,7 @@ function checkValid(toCheck) {
 // ex. util.calc('29 + 1 +') -> Error, too many operators
 //
 // Part 2. Implement support for addition and subtraction.
-
-function plusMinus(expression) {
-  var tokens = expression.split(' ');
-
-  var result = parseFloat(tokens[0]);
-  for (var i = 1; i < tokens.length; i+=2) {
-    if (tokens[i] === '+')
-      result = result + parseFloat(tokens[i+1]);
-    else if (tokens[i] === '-')
-      result = result - parseFloat(tokens[i+1]);
-  }
-  return result;
-}
-
-
+//
 // ex. util.calc('1') -> 1
 // ex. util.calc('-12') -> -12
 // ex. util.calc('3 + 2') -> 5
@@ -85,61 +38,7 @@ function plusMinus(expression) {
 // ex. util.calc('5 * 6 + 8 / 9 * 4.5') -> 34
 // ex. util.calc('1 / 0 + 1 * 0') -> Infinity
 // ex. util.calc('1 / 0 * 0 + 1') -> NaN
-
-function multDiv(expression) {
-
-  //turn into an expression containing only plus and minus
-  //operators happen at odd indexes, we should check those out
-
-
-
-  /*
-  //jumps from operator to operator to operator
-  for (var i = 1; i < tokens.length; i+=2) {
-    //only plus and minuses - base case
-    if (tokens[i] === '+' || tokens[i] === '-') {
-      result += tokens[i-1] + ' ' + tokens[i] + ' ';
-      //check if the next character is the final character
-      //if it's adding together the last two numbers, last token should be added
-      if (i+1 === tokens.length-1) {
-        result += tokens[i+1];
-      }
-    } else if (tokens[i] === '*') {
-      result += parseFloat(tokens[i-1]) * parseFloat(tokens[i+1]);
-
-    } else if (tokens[i] === '/') {
-      result += parseFloat(tokens[i-1]) / parseFloat(tokens[i+1]);
-
-    }
-
-  }
-  */
-
-
-  var tokens = expression.split(' ');
-  var result = '';
-  var i = 1;
-
-
-  if (tokens[i] === '+' || tokens[i] === '-') {
-    i += 2;
-  } else if (tokens[i] === '*') {
-    var prod = tokens[i-1] * tokens[i+1];
-    tokens = tokens.slice(0, i-1).concat([prod], tokens.slice(i+2, tokens.length));
-  } else if (tokens[i] === '/') {
-
-  }
-
-
-
-
-
-  return result;
-
-
-}
-
-
+//
 // Bonus: Implement support for the square root operator.
 // Implement support for the `sqrt` operator. `sqrt` is an operator that takes
 // only one argument (i.e. a unary operator). `sqrt` applied before all other
@@ -155,6 +54,158 @@ function multDiv(expression) {
 // ex. util.calc('sqrt 9 - 3 * 10') -> -27
 // ex. util.calc('10 * sqrt 81') -> 90
 util.calc = function(expression) {
-  checkValid(expression);
-  return plusMinus(expression);
+  //Throw exception if the expression is empty
+  if (expression.length === 0)
+    throw "The expression is empty";
+  var expArr = expression.split(" ");
+  //console.log(expArr);
+  //console.log(expArr[0]);
+  //console.log(parseInt(expArr[0]));
+  //console.log(isNaN(parseInt(expArr[0])));
+
+  //Throw exception if first/last elts are not numbers
+  if (isNaN(parseInt(expArr[0])))
+    throw "The first element is not a number";
+
+  if (isNaN(parseInt(expArr[expArr.length-1])))
+    throw "The last element is not a number";
+
+  //Separate numbers and operators
+  var numberCount = [];
+  var operatorCount = [];
+  for (var i = 0; i < expArr.length; i++) {
+    if(isNaN(parseFloat(expArr[i])))
+      operatorCount.push(expArr[i]);
+    if(!isNaN(parseFloat(expArr[i])))
+      //console.log(parseFloat(expArr[i]));
+      numberCount.push(parseFloat(expArr[i]));
+  }
+
+  //# of numbers should always be # of operators + 1
+  if (operatorCount.length !== numberCount.length - 1)
+    throw "Ratio of operators to numbers is not correct";
+
+  //console.log(numberCount);
+  //console.log(typeof operatorCount[0]);
+
+  //Return first number if expression is only one elt long
+  if (numberCount.length === 1)
+    return numberCount[0];
+
+  // Multiplication and division
+  // numberCount = [1,3,2,5]
+  // operatorCounter = ['+', '/', '-']
+  /*
+  var numberCount2 = [];
+  var operatorCount2 = [];
+  for (var i = 0; i < operatorCount.length; i++) {
+
+    if(i === 0 && (operatorCount[i] === "+" || operatorCount[i] === '-')) {
+      numberCount2.push(numberCount[i]);
+      operatorCount2.push(operatorCount[i]);
+    }
+    if (operatorCount[i] === "*") {
+      console.log(numberCount[i]);
+      console.log(numberCount[i+1]);
+      numberCount2.push(numberCount[i] * numberCount[i+1])
+      console.log(numberCount2);
+    }
+    if (operatorCount[i] === "/")
+      numberCount2.push(numberCount[i] / numberCount[i+1])
+
+    if (i === operatorCount.length-1 && (operatorCount[i] === "+" || operatorCount[i] === '-')) {
+      numberCount2.push(numberCount[i+1]);
+      operatorCount2.push(operatorCount[i]);
+    }
+  }
+
+  console.log(numberCount2);
+  console.log(operatorCount2);
+*/
+
+//multiplication and division
+  //var index = operatorCount.indexOf("*");
+
+
+
+/*
+  //Addition and subtraction
+  var num = numberCount[0];
+  for (var i = 0; i < numberCount.length; i++) {
+    if (operatorCount[i-1] === "+")
+      num += numberCount[i];
+    if (operatorCount[i-1] === "-")
+      num -= numberCount[i];
+  }
+  */
+
+/*
+  for (var i = 0; i < numberCount.length; i++) {
+    if (operatorCount[i] === "+")
+      num += numberCount[i];
+    if (operatorCount[i] === "-")
+      num -= numberCount[i];
+    //if (operatorCount[i-1] === "*")
+      //num *= numberCount[i];
+    //if (operatorCount[i-1] === "/")
+      //num /= numberCount[i];
+    console.log(num);
+  }
+*/
+
+//combining the two arrays -->
+  var allNums = [];
+  for (var i = 0; i < numberCount.length; i++){
+    allNums.push(numberCount[i]);
+    if (operatorCount[i] !== undefined)
+      allNums.push(operatorCount[i]);
+  }
+  //console.log(allNums);
+
+
+  //find first instance of mult or division
+  while (allNums.indexOf("*") !== -1 && allNums.indexOf("/") !== -1) {
+    var index = allNums.indexOf("*");
+    if (allNums.indexOf("*") < allNums.indexOf("/")) {
+      allNums.splice(index-1, 3, allNums[index-1] * allNums[index+1]);
+    } else {
+      allNums.splice(index-1, 3, allNums[index-1] / allNums[index+1]);
+    }
+  }
+
+  while (allNums.indexOf("*") !== -1 ) {
+    var index = allNums.indexOf("*");
+    //console.log(allNums[index-1]*allNums[index+1]);
+    allNums.splice(index-1, 3, allNums[index-1] * allNums[index+1]);
+  }
+
+  while (allNums.indexOf("/") !== -1) {
+    var index = allNums.indexOf("/");
+    //console.log(allNums[index-1]*allNums[index+1]);
+    allNums.splice(index-1, 3, allNums[index-1] / allNums[index+1]);
+  }
+
+  while (allNums.indexOf("+") !== -1 && allNums.indexOf("-") !== -1) {
+    var index = allNums.indexOf("+");
+    if (allNums.indexOf("+") < allNums.indexOf("-")) {
+      allNums.splice(index-1, 3, allNums[index-1] + allNums[index+1]);
+    } else {
+      allNums.splice(index-1, 3, allNums[index-1]-allNums[index+1]);
+    }
+  }
+
+  while (allNums.indexOf("+") !== -1) {
+    var index = allNums.indexOf("+");
+    allNums.splice(index-1, 3, allNums[index-1] + allNums[index+1]);
+  }
+
+  while (allNums.indexOf("-") !== -1) {
+    var index = allNums.indexOf("-");
+    allNums.splice(index-1, 3, allNums[index-1]-allNums[index+1]);
+  }
+
+  //console.log(allNums);
+
+  return allNums[0];
+
 };

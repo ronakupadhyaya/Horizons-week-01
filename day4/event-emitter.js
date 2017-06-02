7c11bad56011df180f68b3f987672fafbd386b75
pwd <brianfakhoury@yahoo.com>
2017-06-01 17:21:22 -0700

    Finish required functions

--- day4/event-emitter.js
+++ day4/event-emitter.js
@@ -35,7 +35,7 @@
 // emitter.on('otherEventName', f2);
 // emitter.listeners // -> {someEventName: [f1,f2], otherEventName: [f1]}
 function EventEmitter() {
-  // YOUR CODE HERE
+  this.listeners = {}
 }
 
 // Takes is a string "eventName" and a callback function "fn"
@@ -52,8 +52,13 @@ function EventEmitter() {
 // emitter.emit('someEvent') // -> prints 'called'
 // emitter.emit('someEvent') // -> prints 'called'
 EventEmitter.prototype.on = function(eventName, fn) {
-  // YOUR CODE HERE
+  if (this.hasOwnProperty(eventName)) {
+    this.listeners[eventName].push(fn)
+  } else {
+    this.listeners[eventName] = [fn]
+  }
 }
+
 
 // Takes is a string "eventName" and a single argument arg
 // It calls each of the listeners registered for the event
@@ -70,7 +75,10 @@ EventEmitter.prototype.on = function(eventName, fn) {
 // emitter.emit('someEvent', 2) // -> prints 'called 2'
 // emitter.emit('someEvent', 'x') // -> prints 'called x'
 EventEmitter.prototype.emit = function(eventName, arg) {
-  // YOUR CODE HERE
+  var fnArray = this.listeners[eventName]
+  for (var fn of fnArray) {
+    fn(arg)
+  }
 }
 
 // Takes is a string "eventName" and a callback function "fn"
@@ -87,7 +95,7 @@ EventEmitter.prototype.emit = function(eventName, arg) {
 // emitter.removeListener('someEvent', log)
 // emitter.emit('someEvent', 1) // -> prints nothing
 EventEmitter.prototype.removeListener = function(eventName, fn) {
-  // YOUR CODE HERE
+  this.listeners[eventName].splice(this.listeners[eventName].indexOf(fn))
 }
 
 // *Bonus*: Takes is a string "eventName" and a callback function "fn"
@@ -103,5 +111,14 @@ EventEmitter.prototype.removeListener = function(eventName, fn) {
 // emitter.emit('someEvent') // -> prints nothing
 // emitter.emit('someEvent') // -> prints nothing
 EventEmitter.prototype.once = function(eventName, fn) {
-  // YOUR CODE HERE
+  var myObj = this
+  var called = false;
+  var newFn = function() {
+    if (!called) {
+      called = true
+      fn()
+      myObj.removeListener(eventName, fn)
+    }
+  }
+  myObj.on(eventName, newFn)
 }

4b6afffa1e9ff3324bd5153b59fe4f8bb9f5a20c
JingyW <wangj2@whitman.edu>
2017-06-02 13:03:46 -0700

    Before day5

--- day4/string_compression.js
+++ day4/string_compression.js
@@ -19,4 +19,40 @@
 // https://www.hackerrank.com/challenges/string-compression
 function compressString(string) {
   // YOUR CODE HERE
+  var strArr = string.split('')
+  var count = 1;
+  var returnArr = [];
+  var lastLetter;
+  var returnStr = "";
+  strArr.forEach(function(letter) {
+    if (letter === lastLetter) {
+      count++
+      returnStr.splice(returnStr.length-1,1,count);
+      console.log(returnArr)
+    } else {
+      lastLetter = letter;
+      count = 1
+      returnStr.push(letter);
+      returnStr.push(count);
+      console.log(returnArr)
+    }
+    console.log(returnArr)
+  })
+  /*
+  for (var key in countObj) {
+    if (countObj[key] === 1) {
+      returnStr += key;
+    } else {
+      returnStr += key;
+      returnStr += countObj[key];
+    }
+  }
+  */
+  returnArr.forEach(function(ele) {
+    if (ele !== 1) {
+      returnStr += ele;
+    }
+  })
+  console.log(returnStr)
+  return returnStr.join('');
 }

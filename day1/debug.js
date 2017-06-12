18990791b149f75c79312d57b990b79a88a4383c
blerude <benjamin.lerude@yale.edu>
2017-06-12 13:44:53 -0700

    Finish rocketship

--- day1/debug.js
+++ day1/debug.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // There's syntax error in this function, use the DevTools Console to find it.
-function functionThatReturnsTrue( {
+function functionThatReturnsTrue() {
   return true;
 }
 
@@ -16,10 +16,11 @@ function functionThatReturnsTrue( {
 // ex. firstWord('abc d  e') -> 'abc'
 function firstWord(string) {
   for (var i = 0; i < string.length; i++) {
-    if (string[0] === ' ') {
-      return string.substring(0, i);
+  	debugger;
+    if (string[i] === ' ') {
+    	console.log(string[i])
+      	return string.substring(0, i);
     }
   }
-
-  return string;
+  return string
 }

d6bcc4d79244825a0f36fd620f1ed508888e8bf3
blerude <benjamin.lerude@yale.edu>
2017-06-13 12:27:46 -0700

    Finish dates and underscore

--- day2/dates.js
+++ day2/dates.js
@@ -4,13 +4,14 @@ window.dates = {};
 
 // Exercise 1. dates.createDate(dateStr)
 // Write a function that takes a properly-formatted date string and returns a JS Date Object from a properly formatted date string.
-// ex. dates.createDate('May 17, 2016 9:00:00') -> Date(2016, 4, 17, 9, 0, 0, 0)
+// ex. dates.createDate('May 17, 2016 9:00:00') -> Date(2016, 5, 17, 9, 0, 0, 0)
 // ex. dates.createDate('2015-03-25') -> Date('2015-03-25')
 // ex. dates.createDate('2015-03-25T12:00:00') -> Date('2015-03-25T12:00:00')
 //
 // hint. see http://www.w3schools.com/js/js_dates.asp
 dates.createDate = function(dateStr) {
-  // YOUR CODE HERE
+	var date = new Date(dateStr);
+	return date
 };
 
 // Exercise 2. dates.getUTCString(dateObj<Date>)
@@ -23,7 +24,8 @@ dates.createDate = function(dateStr) {
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toUTCString
 // hint. see http://www.w3schools.com/js/js_dates.asp
 dates.getUTCString = function(dateObj) {
-  // YOUR CODE HERE
+  	var string = new Date(dateObj).toUTCString();
+  	return string
 };
 
 // Exercise 3.A dates.isSameDayOfWeek(dateObj<Date>, otherDateObj<Date>)
@@ -34,7 +36,12 @@ dates.getUTCString = function(dateObj) {
 //
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
 dates.isSameDayOfWeek = function(dateObj, otherDateObj) {
-  // YOUR CODE HERE
+	if (dateObj.getDay() === otherDateObj.getDay()) {
+ 		return true
+	}
+	else {
+		return false
+	}
 };
 
 // Exercise 3.B dates.isSameTimeOfDay(dateObj<Date>, otherDateObj<Date>)
@@ -47,7 +54,12 @@ dates.isSameDayOfWeek = function(dateObj, otherDateObj) {
 // hint. don't worry about milliseconds!
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getHours
 dates.isSameTimeOfDay = function(dateObj, otherDateObj) {
-  // YOUR CODE HERE
+	if (dateObj.getHours() === otherDateObj.getHours()) {
+ 		return true
+	}
+	else {
+		return false
+	}
 };
 
 // Exercise 3.C dates.isTheFuture(dateObj<Date>)
@@ -55,7 +67,13 @@ dates.isSameTimeOfDay = function(dateObj, otherDateObj) {
 //
 // hint. how do you check if something is 'bigger than' something else?
 dates.isTheFuture = function(dateObj) {
-  // YOUR CODE HERE
+	var now = new Date();
+	if (dateObj > now) {
+		return true
+	}
+	else {
+		return false
+	}
 };
 
 // Exercise 4. dates.incrementDay(dateObj<Date>)
@@ -65,5 +83,6 @@ dates.isTheFuture = function(dateObj) {
 // hint. don't worry about overflow!
 // hint. see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setDate
 dates.incrementDay = function(dateObj) {
-  // YOUR CODE HERE
+	dateObj.setDate(dateObj.getDate() + 1);
+	return dateObj;
 };
--- day2/learn_underscore.js
+++ day2/learn_underscore.js
@@ -42,7 +42,9 @@ learn_underscore.hasZeros = function(array) {
 // ex. learn_underscore.contains(['a'], 'a') -> true
 // ex. learn_underscore.contains(['a', 'b', 'c'], 1) -> false
 learn_underscore.contains = function(array, item) {
-  // YOUR CODE HERE
+  return _.any(array, function(n) {
+    return item === n;
+  })
 };
 
 // Exercise 3: learn_underscore.any(array, fun)
@@ -103,7 +105,10 @@ learn_underscore.contains = function(array, item) {
 //   learn_underscore.any([0, 1, 0], isTruthy) -> true
 //   learn_underscore.any([1], isTruthy) -> true
 learn_underscore.any = function(array, fun) {
-  // YOUR CODE HERE
+  var boolArr = _.map(array, fun);
+  return _.reduce(boolArr, function(a, b) {
+    return a || b;
+  })
 }
 
 // Exercise 4: learn_underscore.reduce(array, fun)
@@ -144,7 +149,11 @@ learn_underscore.any = function(array, fun) {
 //  learn_underscore.reduce([false], and) -> false
 //  learn_underscore.reduce([false, false], and) -> false
 learn_underscore.reduce = function(array, fun) {
-  // YOUR CODE HERE
+  var acc = array[0];
+  for (var i = 1; i < array.length; i++) {
+    acc = fun(acc, array[i])
+  }
+  return acc
 }
 
 // Exercise 5: learn_underscore.keys(object)
@@ -161,7 +170,11 @@ learn_underscore.reduce = function(array, fun) {
 // _.forEach({a: 5, b: 11},
 //           function(value, key) { console.log(value, key) }) -> outputs "5 a" then "11 b"
 learn_underscore.keys = function(object) {
-  // YOUR CODE HERE
+  var keyArr = [];
+  _.forEach(object, function(value, key) {
+    keyArr.push(key);
+  })
+  return keyArr;
 }
 
 // Exercise 6: learn_underscore.values(object)
@@ -171,7 +184,11 @@ learn_underscore.keys = function(object) {
 // ex. learn_underscore.values({}) -> []
 // ex. learn_underscore.values({a: 1, hello: 10}) -> [1, 10]
 learn_underscore.values = function(object) {
-  // YOUR CODE HERE
+  var valArr = [];
+  _.forEach(object, function(value) {
+    valArr.push(value);
+  })
+  return valArr;
 }
 
 // Exercise 7: learn_underscore.pairs(object)
@@ -181,7 +198,11 @@ learn_underscore.values = function(object) {
 // ex. learn_underscore.pairs({}) -> []
 // ex. learn_underscore.pairs({a: 1, hello: 10}) -> [['a', 1], ['hello', 10]]
 learn_underscore.pairs = function(object) {
-  // YOUR CODE HERE
+  var pairs = [];
+  _.forEach(object, function(value, key) {
+    pairs.push([key, value]);
+  })
+  return pairs;
 }
 
 // Example 2: groupByState(people)
@@ -268,5 +289,7 @@ learn_underscore.countLetters = function(string) {
 //  }
 //  learn_underscore.countBy(words, wordLength) -> {4: 1, 5: 3, 2: 1}
 learn_underscore.countBy = function(array, fun) {
-  // YOUR CODE HERE
+  return _.mapObject(_.groupBy(array, fun), function(arr) {
+    return arr.length;
+  })
 }

ea6c3878e36a6d849408bb044154745541332b95
Jack <jdrosenblatt18@gmail.com>
2017-06-16 13:30:10 -0700

    Day5

--- day5/dinosaur.js
+++ day5/dinosaur.js
@@ -11,14 +11,9 @@ game.onReady(function() {
   var score = 0;
   var highScore = game.getHighScore();
   var dead = false;
-  var interval;
   var counter = 0;
 
-  window.requestAnimationFrame(function() {
-    interval = setInterval(eventLoop, 100)
-  });
-
-  function eventLoop() {
+  function eventLoop(timestamp) {
     score++;
     game.clear()
 
@@ -29,35 +24,43 @@ game.onReady(function() {
 
     game.drawScore(score);
     game.drawHighScore(highScore);
-    obstacleX -= 10 + score * .05;
+    obstacle1X -= 10 + score * .005;
+    obstacle2X -= 9 + score * .006;
     game.drawDinosaur(100, dinosaurY);
     game.drawObstacle(obstacle1X, obstacleY)
     game.drawObstacle(obstacle2X, obstacleY)
 
     dinosaurY = dinosaurY - dinosaurVelocity;
 
-    if (obstacleX <= 0)
-      obstacleX = game.width;
+    if (obstacle1X <= 0)
+      obstacle1X = game.width;
+    if (obstacle2X <= 0)
+      obstacle2X = game.width;
     if (dinosaurY < 200) {
-      if (score * .005 > 1.2)
-        dinosaurVelocity -= .5 + 1.2;
+      if (score * .005 > .5)
+        dinosaurVelocity -= .3 + .5;
       else
-        dinosaurVelocity -= .5 + score * .005;
+        dinosaurVelocity -= .3 + score * .005;
     } else if (dinosaurY > 200) {
       dinosaurVelocity = 0;
       dinosaurY = 200;
       counter = 0;
     }
 
-    if (obstacleX < 100 + game.dinosaurWidth && obstacleX + game.obstacleWidth > 100 &&
+    if (obstacle1X < 100 + game.dinosaurWidth && obstacle1X + game.obstacleWidth > 100 &&
+      obstacleY - game.obstacleHeight < dinosaurY - game.dinosaurHeight + game.dinosaurHeight &&
+      game.obstacleHeight + obstacleY - game.obstacleHeight > dinosaurY - game.dinosaurHeight ||
+      obstacle2X < 100 + game.dinosaurWidth && obstacle2X + game.obstacleWidth > 100 &&
       obstacleY - game.obstacleHeight < dinosaurY - game.dinosaurHeight + game.dinosaurHeight &&
       game.obstacleHeight + obstacleY - game.obstacleHeight > dinosaurY - game.dinosaurHeight) {
-
       dead = true;
-      clearInterval(interval);
       game.drawMessage('You lose :(. Press up to restart');
+      window.cancelAnimationFrame();
     }
+    window.requestAnimationFrame(eventLoop);
   }
+
+  window.requestAnimationFrame(eventLoop);
 
   game.onUpArrow(function() {
     if (!dead && counter <= 1) {
@@ -65,14 +68,12 @@ game.onReady(function() {
       counter++;
     } else if (dead) {
       dead = false;
-      window.requestAnimationFrame(function() {
-        interval = setInterval(eventLoop, 100);
-      })
-      obstacleX = 450;
+      window.requestAnimationFrame(eventLoop);
+      obstacle1X = 450;
+      obstacle2X = 450;
       dinosaurY = 200;
       score = 0;
       counter = 0;
     }
   })
-
 });

d14c4c49665948fdc0e29682cf28de3ee9aadbf3
obadiar <ob.diar@gmail.com>
2017-09-05 18:14:43 -0700

    Solve calculator exercise

--- day1/util_calc.js
+++ day1/util_calc.js
@@ -53,6 +53,64 @@ window.util = {};
 // ex. util.calc('-1 * sqrt 4 - 3') -> -5
 // ex. util.calc('sqrt 9 - 3 * 10') -> -27
 // ex. util.calc('10 * sqrt 81') -> 90
+
+1 + 2 * 3
+
 util.calc = function(expression) {
   // YOUR CODE HERE
+  var calculate = function (a, b, operator){
+  	switch(operator) {
+  		case '+': return a+b;
+  		case '-': return a-b;
+  		case '*': return a*b;
+  		case '/': return a/b;
+  	}
+  };
+
+  var trimArray = function(){
+  	for (var i = parsedArray.length-1; i >= 0; i--){
+  		if (parsedArray[i] === '')
+  			parsedArray.splice(i, 1);
+  	}
+  };
+
+  if (expression === "")
+  	throw "Empty expression";
+  var array = expression.split(" ");
+  var operators = [['*', '/'],["+", "-"]];
+  var parsedArray = [];
+  for (var i = 0; i < array.length; i++){
+  	if (!isNaN(parseFloat(array[i])))
+  		parsedArray.push(parseFloat(array[i]));
+  	else
+  		parsedArray.push(array[i]);
+  }
+  if (isNaN(parsedArray[0]) || isNaN(parsedArray[array.length-1]))
+  	throw "Error, operator at the wrong spot";
+  for (var i = 0; i < parsedArray.length; i++){
+  	if ((i % 2 === 0 && isNaN(parsedArray[i]) || (i % 2 !== 0 && !isNaN(parsedArray[i]))))
+  		throw "Error, invalid expression";
+  }
+  if (array.length === 1)
+  	return parseInt(expression);
+  console.log(parsedArray);
+  for (var i = 0; i < 2; i++){
+  	for (var j = 0; j < parsedArray.length; j++){
+  		var operator = operators[i].indexOf(parsedArray[j]);
+  		if (operator !== -1){
+  			 console.log(parsedArray[j-1]);
+  			 console.log(parsedArray[j]);
+  			 console.log(parsedArray[j+1]);
+  			parsedArray[j+1] = calculate(parsedArray[j-1], parsedArray[j+1], operators[i][operator]);
+  			parsedArray[j-1] = '';
+  			parsedArray[j] = '';
+  			console.log(parsedArray[j+1]);
+  		}
+  	}
+  console.log(parsedArray);
+  trimArray();
+  console.log(parsedArray);
+  }
+  trimArray();
+  return parsedArray[0];
 };

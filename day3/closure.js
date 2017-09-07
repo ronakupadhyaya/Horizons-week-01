a29775bebb2cb9581cad4e881caf1afe06977c1d
borajimin <borajimin@gmail.com>
2017-09-07 14:48:48 -0700

    Fix code

--- day3/closure.js
+++ day3/closure.js
@@ -14,7 +14,13 @@
 // based on whether the attempt matches password. The purpose of
 // this function is to hide the password from prying eyes.
 function vault(password) {
-  // YOUR CODE HERE
+  return function fn(attempt){
+    if(attempt === password){
+      return true;
+    } else{
+      return false;
+    }
+  };
 }
 
 // This function returns an object that leaks private information!
@@ -25,10 +31,7 @@ var createUser = function(username, password) {
     // Delete privatePassword and use vault()
     // to implement the login function
     // YOUR CODE HERE
-    privatePassword: password,
-    login: function(attempt) {
-      return this.privatePassword === attempt;
-    }
+    login: vault(password)
   }
 }
 
@@ -81,13 +84,18 @@ var horizons = createUser('horizons', 'horizonites');
 // ex. multiplyNum(6, 7) -> 30
 // ex. exponentiateNum(5, 5) -> 3125
 // ex. exponentiateNum(6, 5) -> 3125
+
 var once = function(f) {
   var called = false; // Let's create a local variable to track if f has been called
+  var pass;
   return function() {
-    if (! called) { // if f hasn't been called yet
-      f(); // call f
+    if (!called) { // if f hasn't been called yet
+      var argue = Array.prototype.slice.call(arguments);
+      pass = f.apply(this, argue);// call f
       called = true; // mark f as called
+      return pass;
     }
+    return pass;
   }
 }
 
@@ -117,11 +125,15 @@ var once = function(f) {
 // functionFactory(0,2) -> [function, function, function]
 var functionFactory = function(num1, num2) {
   var functionArray = [];
-  for (var i = num1; i <= num2; i++) {
-    functionArray[i] = function() {
-      // function that returns i
-      return i;
-    }
+  var num3 = Math.abs(num2 - num1);
+  for (var i = 0; i <= num3; i++, num1++) {
+    (function() {
+      var j = num1;// function that returns i
+      functionArray[i] = function() {
+        return j;
+      }
+    }());
+
   }
 
   return functionArray;

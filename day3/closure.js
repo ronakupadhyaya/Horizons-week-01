f706fcd209228547a22a874303cbf060d5ab3667
bvi1994 <bvi1@ucsc.edu>
2017-09-08 12:02:25 -0700

    Day 3 work

--- day3/closure.js
+++ day3/closure.js
@@ -15,6 +15,9 @@
 // this function is to hide the password from prying eyes.
 function vault(password) {
   // YOUR CODE HERE
+  return function fn(attempt){
+    return attempt === password;
+  }
 }
 
 // This function returns an object that leaks private information!
@@ -22,13 +25,14 @@ function vault(password) {
 var createUser = function(username, password) {
   return {
     username: username,
+    login: vault(password)
     // Delete privatePassword and use vault()
     // to implement the login function
     // YOUR CODE HERE
-    privatePassword: password,
-    login: function(attempt) {
-      return this.privatePassword === attempt;
-    }
+    // privatePassword: password,
+    // login: function(attempt) {
+    //   return this.privatePassword === attempt;
+    //}
   }
 }
 
@@ -83,11 +87,14 @@ var horizons = createUser('horizons', 'horizonites');
 // ex. exponentiateNum(6, 5) -> 3125
 var once = function(f) {
   var called = false; // Let's create a local variable to track if f has been called
+  var returned;
   return function() {
     if (! called) { // if f hasn't been called yet
-      f(); // call f
+      //f(); // call f
+      returned = f.apply(null, arguments);
       called = true; // mark f as called
     }
+    return returned;
   }
 }
 
@@ -117,12 +124,15 @@ var once = function(f) {
 // functionFactory(0,2) -> [function, function, function]
 var functionFactory = function(num1, num2) {
   var functionArray = [];
+  var index = 0;
   for (var i = num1; i <= num2; i++) {
-    functionArray[i] = function() {
-      // function that returns i
-      return i;
-    }
-  }
+    functionArray[index] = (function(i) {
+      return function(){
+        return i;
+      }
+    }(i));
+    index++;
+  } 
 
   return functionArray;
 }
